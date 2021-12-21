71cea1a333d773b6a05280be49a55de287c49593
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-12-22 09:54:38 +1100

    Add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
-use std::io::{self};
+use std::io;
 use std::str::from_utf8;
 
 fn main() -> io::Result<()> {
@@ -15,26 +15,41 @@ fn main() -> io::Result<()> {
     let output_str = from_utf8(&output.stdout).unwrap();
     let lines:Vec<_> = output_str.lines().collect();
 
-    //return as a Result of Vec<str>
-    if lines.len() == 1 {
-        //single module
-        println!("{}", &lines[0]);
-    } else if lines.len() % 2 == 0 {
-        //multimodule, 2 lines per module
-        // line1: moduleName / baseDirectory <- we can ignore this
-        // line2: <actual module path> <- we need this
-        lines
-            .iter()
-            .enumerate()
-            .filter(|(i, _)| (i+1) % 2 == 0) // only get the second line
-            .for_each(|(_, v)| println!("{}", v));
-    } else {
-        //big fat error!
-        println!("invalid output: {:?}", &lines);
-    }
+    let line_result: Result<Vec<String>, String> =
+        if lines.len() == 1 {
+            Ok(vec!(lines[0].trim().to_string()))
+        } else if lines.len() % 2 == 0 {
+            //multimodule, 2 lines per module
+            // line1: moduleName / baseDirectory <- we can ignore this
+            // line2: <actual module path> <- we need this
+            Ok(
+                lines
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| (i+1) % 2 == 0) // only get the second line
+                    .map(|(_, v)| format!("{}", v.trim()))
+                    .collect()
+            )
+        } else {
+            //big fat error!
+            Err(format!("invalid output: {:?}", &lines))
+        };
 
     //map each path to src/main/scala + src/test/scala
     //use Serde to down out to ST project structure as JSON.
 
-    Ok(())
+    match line_result {
+        Ok(lines) => {
+            println!("config: {:?}", lines);
+            Ok(())
+        },
+        Err(error) => {
+            println!("Scoggle config generation failed");
+            Err(to_io_error(&error))
+        }
+    }
+}
+
+fn to_io_error(message: &str) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, message.to_string())
 }
